ae4436d080ead80f3ddfe50cb4755d576aab7de5
Leon Chen <leonchen@Leons-MacBook-Pro.local>
2025-12-09 00:46:43 -1000

    dbehwk

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-      },
-    },
-  },
-});
+  server:
+    mode === "development"
+      ? {
+          port: 5173,
+          proxy: {
+            "/api": {
+              target: "http://localhost:3001",
+              changeOrigin: true,
+            },
+          },
+        }
+      : undefined,
+}));
